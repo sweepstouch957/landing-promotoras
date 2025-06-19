be922554041284f1f819bfdd5c20eab60464defb
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-06-19 06:22:24 -0600

    add first files

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Sweepstouch',
+  description:
+    '¿Eres estudiante universitaria y buscas un trabajo que se adapte a tu horario académico? ¡Esta oportunidad es perfecta para ti!',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
